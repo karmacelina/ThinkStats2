7c77811f362a77fbe917dbbc0ba8d133536052ac
Laura Colon-Melendez <lauradelmar@gmail.com>
2016-09-16 12:00:55 -0400

    Answered chap01ex.py questions

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -13,12 +13,50 @@ import sys
 import nsfg
 import thinkstats2
 
+def ReadFemResp(dct_file = '2002FemResp.dct',
+                dat_file = '2002FemResp.dat.gz'):
+    """ Reads NSFG respondent data. 
+
+    dct_file: string file name
+    dat_file: string file name
+
+    returns: DataFrame
+    """
+
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression = 'gzip')
+    return df
+
+def Validate(df):
+
+    preg = nsfg.ReadFemPreg()
+
+    preg_map = nsfg.MakePregMap(preg)
+
+    for index, pregnum in df.pregnum.iteritems():
+        caseid = df.caseid[index]
+        indices = preg_map[caseid]
+
+        if len(indices) != pregnum:
+            print(caseid, len(indices), pregnum)
+            return False
+
+    return True
 
 def main(script):
     """Tests the functions in this module.
 
     script: string script name
     """
+    resp = ReadFemResp()
+    print(resp.shape)
+
+    assert resp.caseid[2] == 11586
+    assert Validate(resp) == True
+
+    print(resp.pregnum.value_counts().sort_index())
+    print("7 or more pregnancies: %d" % len(resp[resp.pregnum >= 7]))
+
     print('%s: All tests passed.' % script)
 
 
